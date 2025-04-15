d35ff851b12e6b3c3342fd3c7135bf207f714bf3
Carol Br <icarolinebrasil@gmail.com>
2025-04-15 17:57:38 -0300

    Lint fix e add novo bucket

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -4,7 +4,13 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "repo-videos" {
-  provider = aws.us_east
-  bucket = "repo-videos"
+  provider      = aws.us_east
+  bucket        = "repo-videos"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "repo-frame-images" {
+  provider      = aws.us_east
+  bucket        = "repo-frame-images"
   force_destroy = true
 }
--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable "region" {
-    description = "Default AWS region to deploy resources"
-    type        = string
-    default     = "us-east-1"
+  description = "Default AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
 }
